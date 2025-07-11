e0ee1ecca5bb015784b80394877fb34046e6c0e4
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-07-11 11:47:49 +0500

    varible

---
+++ Variable in JS/script.js
@@ -0,0 +1,19 @@
+// var let const 
+
+var name = "bhdfjgh";
+var name = "gnmerlwm"
+console.log(name)
+
+let age;
+age = 44;
+console.log(age);
+
+const pi = 3.14
+console.log(pi);
+
+
+let price1 = 344
+let price2 = 788
+let total = price1+price2
+console.log(total``);
+
