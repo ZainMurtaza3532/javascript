198e1e1f51f73e361fbd05bc2094167f072bf0e6
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-07-11 12:37:33 +0500

    Basics

---
+++ Beginner Level/Basics/script.js
@@ -0,0 +1,27 @@
+console.log("My Name is zain")
+
+// Comments (Single-line and Multi-line)
+
+// This is a single-line comment
+console.log("this is a single line comment");
+
+
+/*This is a 
+ multi-line 
+comment*/
+console.log("This is a multi-line comment");
+
+
+
+let name = "Zain";
+console.log("My name is", name);
+
+
+alert("Welcome to javascript");
+
+prompt ("Enter your name");
+
+
+let isConfirmed = confirm("Are you sure?");
+console.log("User clicked OK?", isConfirmed);
+
